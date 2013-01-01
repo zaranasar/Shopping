5c02eed7c429e01f1915270b11c50f530313311d
zaranasar <bsef09m048@pucit.edu.pk>
2013-01-01 23:44:32 +0500

    First Commit

---
+++ Empty_Site/App_Code/Cart.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassName
+/// </summary>
+public class Cart
+{
+    public int PID;
+    public string name;
+    public string description;
+    public double price;
+    public string path;
+    public int quantity;
+    public Cart(int pid,string n,string d,double p,int quan,string image)
+    {
+        PID=pid;
+        name=n;
+        description=d;
+        price=p;
+        quantity=quan;
+        path=image;
+    }
+}
---
+++ Empty_Site/App_Code/global.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+/// <summary>
+/// Summary description for ClassName
+/// </summary>
+public class Global
+{
+    public static string Exceptions=null;
+    public static List<Cart> list=new List<Cart>();
+    public static void delete(int id)
+    {
+        for(int i=0;i<list.Count;i++)
+        {
+            if(list[i].PID==id)
+            {
+                list.RemoveAt(i);
+                Exceptions=null;
+            }
+        }
+        Exceptions="Record Not Found in the Cart";
+    }
+
+    public static void update(int id,int orgqQuan,int givenQuan)
+    {
+        if(orgqQuan>=givenQuan)
+        {
+            for(int i=0;i<list.Count;i++)
+            {
+                if(list[i].PID==id)
+                {
+                    list.RemoveAt(i);
+                    Exceptions=null;;
+                }
+            }
+        }
+        else
+        {
+            Exceptions="Entered Quantity Exceeds the available Limit";
+        }
+    }
+
+    public static Cart Get(int pid)
+    {
+        foreach(var val in list)
+        {
+            if(val.PID==pid)
+            {
+                return val;
+                Exceptions=null;
+            }
+        }
+        Exceptions="Not found in cart";
+        return null;
+    }
+    public Global()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+}
